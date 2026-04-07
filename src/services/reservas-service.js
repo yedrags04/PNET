c455881d4f004dc2f155d19e2d2de1d0d252c565
Joaquín Guerra <joaquinguerratocino@gmail.com>
2026-04-07 14:21:04 +0200

    fix: restaurar código de reservas :(

--- src/services/reservas-service.js
+++ src/services/reservas-service.js
@@ -1,39 +1,85 @@
 const { getCollection } = require("#src/db.js");
 const { ObjectId } = require("mongodb");
 
+/**
+ * @typedef {Object} ReservaData
+ * @property {string} leaderName
+ * @property {string} leaderEmail
+ * @property {number} experience
+ * @property {string} bankId ObjectId del banco en formato hexadecimal
+ * @property {string} operationDate
+ * @property {string} operationTime
+ * @property {number} teamSize
+ * @property {string} riskLevel
+ * @property {number} budget
+ * @property {string} equipment
+ * @property {string} plan
+ * @property {string[]} specialties
+ * @property {string} status
+ * @property {string} createdAt
+ */
+
+/**
+ * @typedef {ReservaData & { _id: import("mongodb").ObjectId }} Reserva
+ */
+
 class ReservasService {
+    /** @returns {Promise<Reserva[]>} */
     async getAll() {
         const reservasCollection = await getCollection("reservas");
         return await reservasCollection.find({}).toArray();
     }
 
+    /**
+     * @param {string} _id
+     * @returns {Promise<Reserva | null>}
+     */
     async get(_id) {
         const reservasCollection = await getCollection("reservas");
-        return await reservasCollection.findOne({ _id: new ObjectId(_id) });
+        return await reservasCollection.findOne({ _id: ObjectId.createFromHexString(_id) });
     }
 
+    /**
+     * @param {ReservaData | ReservaData[]} data
+     * @returns {Promise<import("mongodb").InsertOneResult<ReservaData> | import("mongodb").InsertManyResult<ReservaData>>}
+     */
     async create(data) {
         const reservasCollection = await getCollection("reservas");
-        // Aseguramos que la fecha de creación se guarde siempre
-        const dataToSave = Array.isArray(data)
-            ? data.map((d) => ({ ...d, createdAt: new Date().toISOString() }))
-            : { ...data, createdAt: new Date().toISOString() };
-
-        if (Array.isArray(dataToSave)) {
-            return await reservasCollection.insertMany(dataToSave);
+        if (Array.isArray(data)) {
+            return await reservasCollection.insertMany(data);
         } else {
-            return await reservasCollection.insertOne(dataToSave);
+            return await reservasCollection.insertOne(data);
         }
     }
 
-    async update(_id, data) {
+    /**
+     * @returns {Promise<import("mongodb").DeleteResult>}
+     */
+    async deleteAll() {
         const reservasCollection = await getCollection("reservas");
-        return await reservasCollection.updateOne({ _id: new ObjectId(_id) }, { $set: data });
+        return await reservasCollection.deleteMany({});
     }
 
+    /**
+     * @param {string} _id
+     * @returns {Promise<import("mongodb").DeleteResult>}
+     */
     async delete(_id) {
         const reservasCollection = await getCollection("reservas");
-        return await reservasCollection.deleteOne({ _id: new ObjectId(_id) });
+        return await reservasCollection.deleteOne({ _id: ObjectId.createFromHexString(_id) });
+    }
+
+    /**
+     * @param {string} _id
+     * @param {Partial<ReservaData>} data
+     * @returns {Promise<import("mongodb").UpdateResult>}
+     */
+    async update(_id, data) {
+        const reservasCollection = await getCollection("reservas");
+        return await reservasCollection.updateOne(
+            { _id: ObjectId.createFromHexString(_id) },
+            { $set: data },
+        );
     }
 }
 
